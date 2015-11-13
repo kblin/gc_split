a6b16a022d2dce30d906091881aef3f6e095e92e
Kai Blin <kblin@biosustain.dtu.dk>
2015-11-13 13:58:44 +0100

    Initial commit
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ README.md
@@ -0,0 +1,35 @@
+gc_split
+========
+
+A simple script to split DNA sequence records into two sets of records with a
+high GC content and a low GC content, respectively.
+
+
+Installation
+------------
+
+Assuming you have BioPython installed in your environment, the script should
+just work. If you prefer to run it with Python 2.x instead of Python 3, change
+the first line of the script from
+```
+#!/usr/bin/env python3
+```
+to
+```
+#!/usr/bin/env python2
+```
+(or whatever the python2 binary is called on your system).
+
+
+Running
+-------
+
+Simply call the script with an input file to get going. `--help` will provide
+all command line options.
+
+
+License
+-------
+
+This code is licensed under the BSD 3-clause license. Find the exact term in the
+`LICENSE` file.
---
+++ gc_split.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import argparse
+from Bio import SeqIO
+from Bio.SeqUtils import GC
+
+_version = '0.1.0'
+
+
+def main():
+    """parse the command line and run"""
+    parser = argparse.ArgumentParser(description="Split a set of FASTA records into high-GC and low-GC records")
+
+    parser.add_argument('infiles', metavar='FILE', nargs='+',
+                        help='File containing one or more sequence records')
+    parser.add_argument('--high', default='high_gc.fa',
+                        help='Filename for high GC records')
+    parser.add_argument('--low', default='low_gc.fa',
+                        help='Filename for low GC records')
+    parser.add_argument('-t', '--threshold', type=float, default=50.0,
+                        help='GC percentage over which to consider records as high GC (default: 50)')
+    parser.add_argument('--version', action='version', version='%(prog)s {}'.format(_version))
+
+    args = parser.parse_args()
+
+    records = load_records(args.infiles)
+    high_gc, low_gc = split_by_gc(records, args.threshold)
+    num_high = SeqIO.write(high_gc, args.high, 'fasta')
+    num_low = SeqIO.write(low_gc, args.low, 'fasta')
+
+    print("Wrote {} high GC and {} low GC records".format(num_high, num_low))
+
+
+
+def load_records(infiles):
+    """Load records specified in infiles and concatenate them"""
+
+    records = []
+
+    for infile in infiles:
+        records.extend(list(SeqIO.parse(infile, 'fasta')))
+
+    return records
+
+
+def split_by_gc(records, threshold=50):
+    """Split records by DNA GC content.
+    All seqs with GC > threshold will go into high_gc, others into low_gc
+    """
+
+    high_gc = []
+    low_gc = []
+
+    for record in records:
+        if GC(record.seq) > threshold:
+            high_gc.append(record)
+        else:
+            low_gc.append(record)
+
+    return high_gc, low_gc
+
+
+if __name__ == '__main__':
+    main()
